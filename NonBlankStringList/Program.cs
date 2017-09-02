173d5c891009d942fd8d3e70fda74bb946c732f3
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-02 23:36:34 +0200

    update

--- NonBlankStringList/Program.cs
+++ NonBlankStringList/Program.cs
@@ -8,7 +8,7 @@ namespace NonBlankStringList
         {
             NonBlankStringList lst = new NonBlankStringList();
             lst.Add("Item added at index 0");
-            lst[0] = "Item changed at index 0";
+            lst[0] = "";
             lst.Add("Item added at index 1");
             lst.Insert(2, "Item inserted at index 2");
 

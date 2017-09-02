83e6c0d197ed2434a8306e2cfe89bdcfb60be775
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-02 23:32:43 +0200

    Tutorial checkpoint

---
+++ NonBlankStringList/NonBlankStringList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace NonBlankStringList
+{
+    class NonBlankStringList : Collection<string>
+    {
+        //inserts non blank/null strings
+        protected override void InsertItem(int index, string item)
+        {
+            if (string.IsNullOrWhiteSpace(item))
+                throw new ArgumentException("Elements of NonBlankStringList" +
+                    "must not be null or white space.");
+            base.InsertItem(index, item);
+        }
+
+        //sets non blank/null strings
+        protected override void SetItem(int index, string item)
+        {
+            if (string.IsNullOrWhiteSpace(item))
+                throw new ArgumentException("Elements of NonBlankStringList" +
+                    "must not be null or white space.");
+            base.SetItem(index, item);
+        }
+    }
+}
--- NonBlankStringList/Program.cs
+++ NonBlankStringList/Program.cs
@@ -6,7 +6,16 @@ namespace NonBlankStringList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            NonBlankStringList lst = new NonBlankStringList();
+            lst.Add("Item added at index 0");
+            lst[0] = "Item changed at index 0";
+            lst.Add("Item added at index 1");
+            lst.Insert(2, "Item inserted at index 2");
+
+            foreach (string item in lst)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
